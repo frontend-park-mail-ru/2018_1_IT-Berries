8d61c6616ffb946c9c2cc8ca02b728a764ce332b
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-03-10 17:40:00 +0300

    JSDoc for API and HTTP modules

--- public/project/modules/api.js
+++ public/project/modules/api.js
@@ -1,21 +1,44 @@
 ;
 
+/**
+ * API module.
+ * @module modules/api
+ */
+
+/** Class representing an API module. */
 export default class ApiModule {
 
+  /**
+   * Create an API module.
+   * @param {HttpModule} httpModule - The http module for api requests.
+   */
   constructor(httpModule) {
     this._httpModule = httpModule;
   }
 
+  /**
+   * Get the httpModule value.
+   * @return {HttpModule} The http module used for api requests.
+   */
   get httpModule() {
     return this._httpModule;
   }
 
+  /**
+   * Set the httpModule value.
+   * @param {HttpModule} httpModule - The http module for api requests.
+   */
   set httpModule(httpModule) {
     this._httpModule = httpModule;
   }
 
   // Authorization and Users API methods
 
+  /**
+   * Fetch api request to load user profile.
+   * @access public
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   loadProfile() {
     if (!this._httpModule) {
       return;
@@ -24,6 +47,11 @@ export default class ApiModule {
     return this._httpModule.fetchGet('/profile-data');
   }
 
+  /**
+   * Fetch api request to load users.
+   * @access public
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   loadUsers(listSize = 5, listNumber = 1) {
     if (!this._httpModule) {
       return;
@@ -32,6 +60,11 @@ export default class ApiModule {
     return this._httpModule.fetchGet('/users?listSize=' + listSize + '&listNumber=' + listNumber);
   }
 
+  /**
+   * Fetch api request to load information about current user.
+   * @access public
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   loadMe() {
     if (!this._httpModule) {
       return;
@@ -40,6 +73,12 @@ export default class ApiModule {
     return this._httpModule.fetchGet('/me');
   }
 
+  /**
+   * Fetch api request sign up new user.
+   * @access public
+   * @param {FormData} user={} - New user data.
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   signupUser(user = {}) {
     if (!this._httpModule) {
       return;
@@ -51,6 +90,12 @@ export default class ApiModule {
     });
   }
 
+  /**
+   * Fetch api request log in user.
+   * @access public
+   * @param {FormData} user={} - User data to log in.
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   loginUser(user = {}) {
     if (!this._httpModule) {
       return;
@@ -62,6 +107,11 @@ export default class ApiModule {
     });
   }
 
+  /**
+   * Fetch api request log out current user.
+   * @access public
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   logOut() {
     if (!this._httpModule) {
       return;
@@ -69,6 +119,12 @@ export default class ApiModule {
     return this._httpModule.fetchGet('/logout');
   }
 
+  /**
+   * Fetch api request to change user profile information.
+   * @access public
+   * @param {FormData} user={} - Changed user profile data.
+   * @return {Promise} A promise that resolves with the result of API request.
+   */
   changeUserData(user = {}) {
     if (!this._httpModule) {
       return;
--- public/project/modules/http.js
+++ public/project/modules/http.js
@@ -1,20 +1,45 @@
 ;
 
+/**
+ * HTTP module.
+ * @module modules/http
+ */
+
+/** Class representing an HTTP module. */
 export default class HttpModule {
 
+  /**
+   * Create an HTTP module.
+   * @param {string} baseUrl='' - The base url for http requests.
+   */
   constructor(baseUrl = '') {
     this._baseUrl = baseUrl;
   }
 
+  /**
+   * Get the baseUrl value.
+   * @return {string} The baseUrl value.
+   */
   get baseUrl() {
     return this._baseUrl;
   }
 
+  /**
+   * Set the baseUrl value.
+   * @param {string} baseUrl - The base url for http requests.
+   */
   set baseUrl(baseUrl) {
     this._baseUrl = baseUrl;
   }
 
-  checkStatus(response) {
+  /**
+   * Check status of http response.
+   * @access private
+   * @param {Response} response - fetch api response object to check.
+   * @return {Response} The response value.
+   * @throws {Error} Will throw an error if the response status is not Success (2xx).
+   */
+  _checkStatus(response) {
     if (response.status >= 200 && response.status < 300) {
       return response;
     } else {
@@ -24,7 +49,13 @@ export default class HttpModule {
     }
   }
 
-  parseJSON(response) {
+  /**
+   * Parse response body.
+   * @access private
+   * @param {Response} response - fetch api response object to parse.
+   * @return {Promise} A promise that resolves with the result of parsing the response text as JSON or text.
+   */
+  _parseResponseBody(response) {
     const contentType = response.headers.get('content-type');
     if (contentType && contentType.indexOf('application/json') !== -1) {
       return response.json();
@@ -33,6 +64,12 @@ export default class HttpModule {
     }
   }
 
+  /**
+   * Provides async HTTP GET request with CORS supported.
+   * @access public
+   * @param {string} path='/' - path for http request.
+   * @return {Promise} A promise that resolves with the result of HTTP GET request.
+   */
   fetchGet(path = '/') {
     const url = this._baseUrl + path;
     const options = {
@@ -42,14 +79,22 @@ export default class HttpModule {
     };
 
     return fetch(url, options)
-      .then(this.checkStatus)
-      .then(this.parseJSON)
+      .then(this._checkStatus)
+      .then(this._parseResponseBody)
       .catch( error => {
         throw error;
       });
 
   }
 
+  /**
+   * Provides async HTTP POST request with CORS supported.
+   * @access public
+   * @param {Object} object={} - the object of request params.
+   * @param {string} object.path='/' - path for http request.
+   * @param {string} object.formData={} - the form data for request body.
+   * @return {Promise} A promise that resolves with the result of HTTP POST request.
+   */
   fetchPost({path = '/', formData = {}}) {
     const url = this._baseUrl + path;
     const options = {
@@ -59,8 +104,8 @@ export default class HttpModule {
       body: formData
     };
     return fetch(url, options)
-      .then(this.checkStatus)
-      .then(this.parseJSON)
+      .then(this._checkStatus)
+      .then(this._parseResponseBody)
       .catch( error => {
         throw error;
       });
