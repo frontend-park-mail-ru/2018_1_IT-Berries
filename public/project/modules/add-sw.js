093d33b3aa4b88ef6823ac3402544329888e9b38
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-04-11 21:47:09 +0300

    service worker added but does not work:(

---
+++ public/project/modules/add-sw.js
@@ -0,0 +1,11 @@
+export function addServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', {scope: '/'})
+      .then((registration) => {
+        console.log('sw registration on scope:', registration.scope);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
