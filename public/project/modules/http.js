528078030984ed6bce8a90cfa3995418089678c5
Anastasia Puchnina <puchnina.nastya@gmail.com>
2018-04-13 19:18:23 +0300

    noda

--- public/project/modules/http.js
+++ public/project/modules/http.js
@@ -15,7 +15,9 @@ class HttpModule {
       this._baseUrl = 'http://localhost:8081';
       break;
     case 'itberries-frontend.herokuapp.com':
-      this._baseUrl = 'https://itberries-backend.herokuapp.com';
+      this._baseUrl = 'https://itberries-frontend.herokuapp.com:8081';
+
+      //this._baseUrl = 'https://itberries-backend.herokuapp.com';
       break;
     }
   }
